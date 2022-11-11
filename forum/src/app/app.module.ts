19f8b8ac307bee037db4c9423358fac7e93110e2
Iglika Mincheva <103664008+Lika-M@users.noreply.github.com>
2022-11-11 14:42:20 +0200

    Add theme-list and post-list components in app.module.

--- forum/src/app/app.module.ts
+++ forum/src/app/app.module.ts
@@ -3,11 +3,15 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-import {CoreModule} from './core/core.module'
+import {CoreModule} from './core/core.module';
+import { TeamListComponent } from './team-list/team-list.component';
+import { PostListComponent } from './post-list/post-list.component';
 
 @NgModule({
   declarations: [
     AppComponent,
+    TeamListComponent,
+    PostListComponent,
   ],
   imports: [
     BrowserModule,
